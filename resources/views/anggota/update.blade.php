9fc7675c9ad64936887f651fd2426666e9f3545c
silviansi <silviansi24@gmail.com>
2024-06-09 20:09:01 +0700

    fix: label for

--- resources/views/anggota/update.blade.php
+++ resources/views/anggota/update.blade.php
@@ -12,49 +12,49 @@
                             @csrf
                             @method("PUT")
                                 <div class="row mb-3">
-                                    <label for="example-text-input" class="col-sm-2 col-form-label">Nama</label>
+                                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="nama" type="text" value="{{ $item->user->nama }}" id="nama">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-search-input" class="col-sm-2 col-form-label">Kebun</label>
+                                    <label for="kebun" class="col-sm-2 col-form-label">Kebun</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="kebun" type="text" value="{{ $item->kebun }}" id="kebun">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-url-input" class="col-sm-2 col-form-label">Luas Kebun</label>
+                                    <label for="luas_kebun" class="col-sm-2 col-form-label">Luas Kebun</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="luas_kebun" type="text" value="{{ $item->luas_kebun }}" id="luas_kebun">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-email-input" class="col-sm-2 col-form-label">No. Vak</label>
+                                    <label for="no_vak" class="col-sm-2 col-form-label">No. Vak</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="no_vak" type="text" value="{{ $item->no_vak }}" id="no_vak">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-tel-input" class="col-sm-2 col-form-label">No. Kontrak</label>
+                                    <label for="no_kontrak" class="col-sm-2 col-form-label">No. Kontrak</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="no_kontrak" type="text" value="{{ $item->no_kontrak }}" id="no_kontrak">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-password-input" class="col-sm-2 col-form-label">Kategori</label>
+                                    <label for="kategori" class="col-sm-2 col-form-label">Kategori</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="kategori" type="text" value="{{ $item->kategori }}" id="kategori">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-number-input" class="col-sm-2 col-form-label">Kecamatan</label>
+                                    <label for="kecamatan" class="col-sm-2 col-form-label">Kecamatan</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="kecamatan" type="text" value="{{ $item->kecamatan }}" id="kecamatan">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
-                                    <label for="example-url-input" class="col-sm-2 col-form-label">Kota</label>
+                                    <label for="kota" class="col-sm-2 col-form-label">Kota</label>
                                     <div class="col-sm-10">
                                         <input class="form-control" name="kota" type="text" value="{{ $item->kota }}" id="kota">
                                     </div>
