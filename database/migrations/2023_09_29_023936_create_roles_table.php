949e64e063206a47c3c59cf4d331156fe3ca26fc
silviansi <silviansi24@gmail.com>
2024-06-12 12:48:14 +0700

    change table roles

--- database/migrations/2023_09_29_023936_create_roles_table.php
+++ database/migrations/2023_09_29_023936_create_roles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('nama', 100);
             $table->timestamps();
         });
     }
