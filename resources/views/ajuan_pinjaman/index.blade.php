64820b86ca37361aadddcbdbd3991be8ae70eee6
silviansi <silviansi24@gmail.com>
2024-06-12 12:55:43 +0700

    fix layout and delete message

--- resources/views/ajuan_pinjaman/index.blade.php
+++ resources/views/ajuan_pinjaman/index.blade.php
@@ -1,66 +1,41 @@
-@extends('layout/depan')
+@extends('layout/base')
 @section('title', 'Detail Pinjaman | Website')
 @section('konten')
     
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="d-flex align-items-center">
-
     <div class="container" style="padding-top: 50px">
         <div class="row">
             <div class="col-lg-12 flex-column justify-content-center">
-                    <div class="card">
-                        <div class="card-body">
-                            @if (Session::has('success'))
-                                <div class="alert alert-success">
-                                    {{ Session::get('success') }}
-                                </div>
-                                    
-                                @endif
-                                @if($errors->any())
-                                    <div class="alert alert-danger">
-                                        <ul>
-                                        @foreach ($errors->all() as $item)
-                                            <li>{{ $item }}</li>
-                                        @endforeach
-                                        </ul>
-                                    </div>
-    
-                                @endif
-
-                            <h4 class="card-title">Daftar Pinjaman Anda</h4>
-
-                            <table style="width:100%; text-align:center">
-                                <thead>
-                                        <tr>
-                                            <th>Tanggal</th>
-                                            <th>No. Bukti</th>
-                                            <th>No. Kontrak</th>
-                                            <th>Jumlah Pinjaman</th>
-                                            <th>Uraian</th>
-                                        </tr>
-                                </thead>
-                                    <tbody>
-                                        @foreach ($data as $item)
-                                            <tr>
-                                                <td>{{ $item->tanggal }}</td>
-                                                <td>{{ $item->no_bukti }}</td>
-                                                <td>{{ $item->profile->no_kontrak }}</td>
-                                                <td>{{ number_format($item->jumlah_pinjaman,0,'','.') }}</td>
-                                                <td>{{ $item->uraian }}</td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                            </table>
-
-                        </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Daftar Pinjaman Anda</h4>
+                        <table style="width:100%; text-align:center">
+                            <thead>
+                                <tr>
+                                    <th>Tanggal</th>
+                                    <th>No. Bukti</th>
+                                    <th>No. Kontrak</th>
+                                    <th>Jumlah Pinjaman</th>
+                                    <th>Uraian</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                <tr>
+                                    <td>{{ $item->tanggal }}</td>
+                                    <td>{{ $item->no_bukti }}</td>
+                                    <td>{{ $item->profile->no_kontrak }}</td>
+                                    <td>{{ number_format($item->jumlah_pinjaman,0,'','.') }}</td>
+                                    <td>{{ $item->uraian }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                </div> <!-- end col -->
-          
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-
-  </section><!-- End Hero -->
-
-    
+</section>
 @endsection
