f9743c5d8e0f13945b8a38c8f875b83efbb13ad4
silviansi <silviansi24@gmail.com>
2024-06-13 11:31:23 +0700

    fix: content help

--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -1,35 +1,32 @@
 @extends('layout/aplikasi')
 @section('title', 'Help | DashLoan')
 @section('konten')
-<!-- ============================================================== -->
-    <!-- Start right Content here -->
-    <!-- ============================================================== -->
-    <div class="main-content">
+<div class="main-content">
+    <div class="page-content">
+        <div class="container-fluid">
 
-        <div class="page-content">
-            <div class="container-fluid">
-
-                <!-- start page title -->
-                <div class="page-title-box">
-                    <div class="row align-items-center">
-                        <div class="col-md-8">
-                            <h6 class="page-title">Help</h6>
-                            <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item active">Berikut Panduan Menggunakan DashLoan</li>
-                            </ol>
-                        </div>
+            <div class="page-title-box">
+                <div class="row align-items-center">
+                    <div class="col-md-8">
+                        <h6 class="page-title">Help</h6>
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item active">Berikut Panduan Menggunakan DashLoan</li>
+                        </ol>
                     </div>
                 </div>
-                <!-- end page title -->
-                <div class="card">
-                    <div class="card-body">
-                        <p>1. Input/Approve data pinjaman dari Anggota</p>
-                        <p>2. Input data Pabrikasi</p>
-                        <p>3. Input data Tutupan</p>
-                        <p>4. Cetak Kartu di halaman Data Anggota</p>
-                    </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <p>1. Mitra Petani membuat akun dengan mengisi form registrasi</p>
+                    <p>2. Admin memasukkan Informasi / Data Tambahan pada Data Mitra, pastikan Data Mitra sudah terisi semua</p>
+                    <p>3. Admin memasukkan data Pinjaman</p>
+                    <p>4. Admin memasukkan data Laporan Harian</p>
+                    <p>5. Admin memasukkan data Tutupan</p>
+                    <p>6. Cetak Kartu di halaman Data Anggota</p>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
